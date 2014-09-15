9621cf3a0317267e6b31de8cb66d08d72c782fce
geza <geza_nagy@epam.com>
2014-09-16 01:47:44 +0200

    add flow diagram

---
+++ docs/design_diagrams/user_flow.mm
@@ -0,0 +1,282 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1410805191086" ID="ID_1829686687" MODIFIED="1410824693763" STYLE="bubble" TEXT="User">
+<node CREATED="1410806489800" ID="ID_1070278224" MODIFIED="1410824693763" POSITION="right" STYLE="bubble" TEXT="Login Screen">
+<icon BUILTIN="full-1"/>
+<node CREATED="1410806496746" ID="ID_1014803694" MODIFIED="1410824693763" STYLE="bubble" TEXT="Static content">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1410806511059" ID="ID_1843431390" MODIFIED="1410824693763" STYLE="bubble" TEXT="Links">
+<icon BUILTIN="full-1"/>
+<node CREATED="1410805990567" ID="ID_1081121139" MODIFIED="1410824693763" STYLE="bubble" TEXT="Registration Form">
+<icon BUILTIN="full-1"/>
+<node CREATED="1410806004057" ID="ID_1035741346" MODIFIED="1410824693764" STYLE="bubble" TEXT="Static content">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1410806037448" ID="ID_487049411" MODIFIED="1410824693764" STYLE="bubble" TEXT="No Controller for show"/>
+<node CREATED="1410806055834" ID="ID_1780621278" MODIFIED="1410824693764" STYLE="bubble" TEXT="Submit">
+<icon BUILTIN="full-2"/>
+<node CREATED="1410806084152" ID="ID_1963707381" MODIFIED="1410824693764" STYLE="bubble" TEXT="User Controller">
+<node CREATED="1410806092837" ID="ID_62985537" MODIFIED="1410824693764" STYLE="bubble" TEXT="Validation">
+<icon BUILTIN="full-3"/>
+<node CREATED="1410806354224" ID="ID_747629170" MODIFIED="1410824693764" STYLE="bubble" TEXT="Validation Service">
+<node CREATED="1410806369805" ID="ID_1078701013" MODIFIED="1410824693764" STYLE="bubble" TEXT="Insert user to DB">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1410806710796" ID="ID_903489459" MODIFIED="1410824693764" STYLE="bubble" TEXT="Forgotten password form">
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410806721183" ID="ID_957233059" MODIFIED="1410824693764" STYLE="bubble" TEXT="User Service">
+<node CREATED="1410806743055" ID="ID_1036616521" MODIFIED="1410824693764" STYLE="bubble" TEXT="Get user details">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1410806750799" ID="ID_710860901" MODIFIED="1410824693764" STYLE="bubble" TEXT="Send email to the user">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1410806525708" ID="ID_31337680" MODIFIED="1410824693764" STYLE="bubble" TEXT="Submit">
+<icon BUILTIN="full-2"/>
+<node CREATED="1410806534371" ID="ID_1744364893" MODIFIED="1410824693764" STYLE="bubble" TEXT="Spring Security">
+<node CREATED="1410806558774" ID="ID_172059542" MODIFIED="1410824693764" STYLE="bubble" TEXT="Error message">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410806572474" ID="ID_671544374" MODIFIED="1410824693764" STYLE="bubble" TEXT="User Service - get user details">
+<icon BUILTIN="button_ok"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1410806396109" ID="ID_449170539" MODIFIED="1410824693764" STYLE="bubble" TEXT="User home">
+<icon BUILTIN="full-1"/>
+<node CREATED="1410806413333" ID="ID_1937644152" MODIFIED="1410824693764" STYLE="bubble" TEXT="Links">
+<icon BUILTIN="full-1"/>
+<node CREATED="1410806903253" ID="ID_1474885100" MODIFIED="1410824693764" STYLE="bubble" TEXT="User Profile">
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410807013782" ID="ID_720145034" MODIFIED="1410824693764" STYLE="bubble" TEXT="User Service - get user details">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+<node CREATED="1410806907981" ID="ID_1779779649" MODIFIED="1410824693765" STYLE="bubble" TEXT="Log out">
+<icon BUILTIN="full-2"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410806919685" ID="ID_831674091" MODIFIED="1410824693765" STYLE="bubble" TEXT="User Service - Kill session">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+</node>
+<node CREATED="1410807079212" ID="ID_1949986551" MODIFIED="1410824693765" STYLE="bubble" TEXT="Dashboard">
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410807124208" ID="ID_696592303" MODIFIED="1410824693765" STYLE="bubble" TEXT="User Service - get user&apos;s games">
+<icon BUILTIN="full-3"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410807149200" ID="ID_701721680" MODIFIED="1410824693765" STYLE="bubble" TEXT="Tournament Service -get Ticket info for every ticket">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1410820120770" ID="ID_1292943148" MODIFIED="1410824693765" STYLE="bubble" TEXT="Tickets">
+<icon BUILTIN="full-1"/>
+<node CREATED="1410820133859" ID="ID_1987448888" MODIFIED="1410824693765" STYLE="bubble" TEXT="Click event">
+<icon BUILTIN="full-2"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410820146608" ID="ID_1284769517" MODIFIED="1410824693765" STYLE="bubble" TEXT="Tournament Service - get tournament details">
+<icon BUILTIN="full-3"/>
+<node CREATED="1410820640362" ID="ID_247996824" MODIFIED="1410824693765" STYLE="bubble" TEXT="Game Portal - User view">
+<icon BUILTIN="full-1"/>
+<node CREATED="1410821177014" ID="ID_625210678" MODIFIED="1410824693765" STYLE="bubble" TEXT="Tips to go">
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410821379290" ID="ID_1192961250" MODIFIED="1410824693765" STYLE="bubble" TEXT="User service - get last tip">
+<icon BUILTIN="full-3"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410821414082" ID="ID_622699003" MODIFIED="1410824693765" STYLE="bubble" TEXT="Fixtures service - get next round matches">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+<node CREATED="1410821293801" ID="ID_95428628" MODIFIED="1410824693765" STYLE="bubble" TEXT="Game Standings">
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410821471210" ID="ID_939439447" MODIFIED="1410824693765" STYLE="bubble" TEXT="Game Service - get Game Standings">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+<node CREATED="1410821305669" ID="ID_1089541485" MODIFIED="1410824693765" STYLE="bubble" TEXT="Standings of the Tournament">
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410821511086" ID="ID_1792918795" MODIFIED="1410824693765" STYLE="bubble" TEXT="Tournament Service - get information by round/group">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+<node CREATED="1410821314315" ID="ID_666291134" MODIFIED="1410824693765" STYLE="bubble" TEXT="Fixtures">
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410821785323" ID="ID_1601582067" MODIFIED="1410824693765" STYLE="bubble" TEXT="Fixtures Service - get Matches by round/group">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+</node>
+<node CREATED="1410820659837" ID="ID_616998057" MODIFIED="1410824693766" STYLE="bubble" TEXT="Game Portal - Admin View">
+<icon BUILTIN="full-1"/>
+<node CREATED="1410822562934" ID="ID_925233397" MODIFIED="1410824693766" STYLE="bubble" TEXT="Result of recent matches modal/section">
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410823067815" ID="ID_1357680530" MODIFIED="1410824693766" STYLE="bubble" TEXT="Fixture Service  - Get matches without result till date">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1410823165632" ID="ID_1668709692" MODIFIED="1410824693766" STYLE="bubble" TEXT="Submit">
+<icon BUILTIN="full-2"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410823171878" ID="ID_1807490034" MODIFIED="1410824693766" STYLE="bubble" TEXT="Tournament Service - Save match results">
+<icon BUILTIN="full-3"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410823222435" ID="ID_1519428960" MODIFIED="1410824693766" STYLE="bubble" TEXT="Game Service - Refresh Game standings">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+</node>
+<node CREATED="1410822614249" ID="ID_1265802712" MODIFIED="1410824693766" STYLE="bubble" TEXT="Game Standings">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1410822694552" ID="ID_863340964" MODIFIED="1410824693766" STYLE="bubble" TEXT="Fixtures">
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410823254112" ID="ID_46541126" MODIFIED="1410824693766" STYLE="bubble" TEXT="Fixture Service - Get matches by round/group">
+<icon BUILTIN="full-3"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410823381452" ID="ID_526623337" MODIFIED="1410824693766" STYLE="bubble" TEXT="Fixture Service - Prefill Team selectors">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1410823165632" ID="ID_392210908" MODIFIED="1410824693766" STYLE="bubble" TEXT="Submit">
+<icon BUILTIN="full-2"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410823171878" ID="ID_788792276" MODIFIED="1410824693766" STYLE="bubble" TEXT="Tournament Service - Save match results">
+<icon BUILTIN="full-3"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410823222435" ID="ID_1073537141" MODIFIED="1410824693766" STYLE="bubble" TEXT="Game Service - Refresh Game standings">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+<node CREATED="1410823408661" ID="ID_1120796435" MODIFIED="1410824693766" STYLE="bubble" TEXT="Change team in match">
+<icon BUILTIN="full-2"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410823421797" ID="ID_1751304541" MODIFIED="1410824693766" STYLE="bubble" TEXT="Fixture Service - Recalculate/Refresh fixtures">
+<icon BUILTIN="full-3"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410823707842" ID="ID_988618105" MODIFIED="1410824693766" STYLE="bubble" TEXT="Validation Service - Validate new Fixture">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+</node>
+<node CREATED="1410821305669" ID="ID_782112424" MODIFIED="1410824693766" STYLE="bubble" TEXT="Standings of the Tournament">
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410821511086" ID="ID_1807981883" MODIFIED="1410824693766" STYLE="bubble" TEXT="Tournament Service - get information by round/group">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1410807085060" ID="ID_1358543515" MODIFIED="1410824693767" STYLE="bubble" TEXT="Notification bar">
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410807363843" ID="ID_1465952267" MODIFIED="1410824693767" STYLE="bubble" TEXT="Message Service - get recent notifications">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1410807423207" ID="ID_1135071336" MODIFIED="1410824693767" STYLE="bubble" TEXT="Click - redirect to specific place which stored with message">
+<icon BUILTIN="full-2"/>
+</node>
+</node>
+<node CREATED="1410823510819" ID="ID_240547551" MODIFIED="1410824693767" STYLE="bubble" TEXT="Refresh frequently">
+<icon BUILTIN="full-2"/>
+</node>
+</node>
+<node CREATED="1410823608161" ID="ID_332856388" MODIFIED="1410824693767" STYLE="bubble" TEXT="Create Game">
+<icon BUILTIN="full-1"/>
+<node CREATED="1410823782016" ID="ID_701314114" MODIFIED="1410824693767" STYLE="bubble" TEXT="Basic Details">
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410823852939" ID="ID_1998727810" MODIFIED="1410824693767" STYLE="bubble" TEXT="Tournament Service - get Tournament types">
+<icon BUILTIN="full-3"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410823876094" ID="ID_1110359137" MODIFIED="1410824693767" STYLE="bubble" TEXT="Validation Service - validate tournament name">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+<node CREATED="1410823792217" ID="ID_622134811" MODIFIED="1410824693767" STYLE="bubble" TEXT="Groups and Teams">
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410823905525" ID="ID_1856467732" MODIFIED="1410824693767" STYLE="bubble" TEXT="Tournament Service - get Tournament Structure">
+<icon BUILTIN="full-3"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410823944856" ID="ID_1545901440" MODIFIED="1410824693767" STYLE="bubble" TEXT="Validation Service - check team duplications/group details">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+<node CREATED="1410823814060" ID="ID_1658844738" MODIFIED="1410824693767" STYLE="bubble" TEXT="Fixtures">
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410823985091" ID="ID_1741573019" MODIFIED="1410824693767" STYLE="bubble" TEXT="Fixture Service - Calculate Fixture">
+<icon BUILTIN="full-3"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410824053878" ID="ID_5857607" MODIFIED="1410824693767" STYLE="bubble" TEXT="Fixture Service - prefill dropdowns">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1410824004351" ID="ID_136571427" MODIFIED="1410824693767" STYLE="bubble" TEXT="Change Match details">
+<icon BUILTIN="full-2"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410824019041" ID="ID_825969340" MODIFIED="1410824693767" STYLE="bubble" TEXT="Fixture Service - Recalculate Fixture">
+<icon BUILTIN="full-3"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410824034499" ID="ID_1684299640" MODIFIED="1410824693767" STYLE="bubble" TEXT="Validation Service - Validate new Fixture">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+</node>
+<node CREATED="1410823825604" ID="ID_354873027" MODIFIED="1410824693768" STYLE="bubble" TEXT="Rules">
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410824077321" ID="ID_1323618839" MODIFIED="1410824693768" STYLE="bubble" TEXT="Game Service - get Default rules">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1410824088456" ID="ID_267252169" MODIFIED="1410824693768" STYLE="bubble" TEXT="Submit">
+<icon BUILTIN="full-2"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410824100693" ID="ID_497639647" MODIFIED="1410824693768" STYLE="bubble" TEXT="Validation Service - Validate rules">
+<icon BUILTIN="full-3"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410824120710" ID="ID_1764984612" MODIFIED="1410824693768" STYLE="bubble" TEXT="Game Service - Save rules">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+</node>
+<node CREATED="1410823841547" ID="ID_1837103091" MODIFIED="1410824693768" STYLE="bubble" TEXT="Attendes">
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410824140096" ID="ID_301410171" MODIFIED="1410824693768" STYLE="bubble" TEXT="User Service - Get user by name">
+<icon BUILTIN="full-3"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410824154364" ID="ID_1990936794" MODIFIED="1410824693768" STYLE="bubble" TEXT="Validation Service - Validate attendes (e.g.: at least two attendes, no duplicates, etc)">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+<node CREATED="1410824198857" ID="ID_1487771921" MODIFIED="1410824693768" STYLE="bubble" TEXT="Submit">
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410824205293" ID="ID_1063685335" MODIFIED="1410824693768" STYLE="bubble" TEXT="Tournament Service - Commit">
+<icon BUILTIN="full-3"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410824240768" ID="ID_1068211394" MODIFIED="1410824693768" STYLE="bubble" TEXT="Game Service - Commit">
+<icon BUILTIN="full-3"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1410824247800" ID="ID_1573831690" MODIFIED="1410824785324" STYLE="bubble" TEXT="Message Service - Send notification to attendes">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1 Module
+    </p>
+    <p>
+      2 Event
+    </p>
+    <p>
+      3 Method call
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
